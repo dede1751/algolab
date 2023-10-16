670f6fb0064fbdd0b6a04d7ccdef300d7b751e0c
dede1751 <63297263+dede1751@users.noreply.github.com>
2023-10-16 23:23:24 +0200

    JamesBondSovereigns 100 with stupid optimizations

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ is guaranteed. The script is meant to be used as specified by its documentation.
 | [Beach Bars](Week2/BeachBars/this.pdf)                     | [beach_bars.cpp](Week2/BeachBars/src/algorithm.cpp)                             | 100       | Week 2    | SW                       |
 | [The Great Game](Week2/TheGreatGame/this.pdf)              | [the_great_game.cpp](Week2/TheGreatGame/src/algorithm.cpp)                      | 100       | Week 2    | Minimax + DP             |
 | [Lord Voldemort](Week2/LordVoldemort/this.pdf)             | [lord_voldemort.cpp](Week2/LordVoldemort/src/algorithm.cpp)                     | 100       | Week 2    | DP + Precompute          |
-| [JamesBondSovereigns](Week3/JamesBondSovereigns/this.pdf)  | [james_bond_sovereigns.cpp](Week3/JamesBondSovereigns/src/algorithm.cpp)        |  75       | POTW 3    | DP                       |
+| [JamesBondSovereigns](Week3/JamesBondSovereigns/this.pdf)  | [james_bond_sovereigns.cpp](Week3/JamesBondSovereigns/src/algorithm.cpp)        | 100       | POTW 3    | DP (broken requirements) |
 | [FirstStepsBGL](Week3/FirstStepsBGL/this.pdf)              | [first_steps_bgl.cpp](Week3/FirstStepsBGL/src/algorithm.cpp)                    | 100       | Week 3    | Dijkstra + MST           |
 | [BuddySelection](Week3/BuddySelection/this.pdf)            | [buddy_selection.cpp](Week3/BuddySelection/src/algorithm.cpp)                   | 100       | Week 3    | Maximum Matching         |
 | [ImportantBridges](Week3/ImportantBridges/this.pdf)        | [important_bridges.cpp](Week3/ImportantBridges/src/algorithm.cpp)               | 100       | Week 3    | Biconnected Components   |
--- Week3/JamesBondSovereigns/src/algorithm.cpp
+++ Week3/JamesBondSovereigns/src/algorithm.cpp
@@ -7,44 +7,42 @@
 #include <algorithm>
 #include <vector>
 
-typedef std::vector<std::vector<int>> MEMO;
-MEMO memo(2001, std::vector<int>(2001, -1));
-std::vector<int> v(2001, 0);
+int memo[2001][2001];
+int v[2001];
 
 int search(const int i, const int j, const int p, const int m, const int k) {
-	if (i == j)
-		return p == k ? v[i] : 0;
-	else if (memo[i][j] != -1)
-		return memo[i][j];
+  if (i == j)
+    return p == k ? v[i] : 0;
+  else if (memo[i][j] != -1)
+    return memo[i][j];
 
-	int next = (p + 1) % m;
-	int rest_first = search(i + 1, j, next, m, k), rest_end = search(i, j - 1, next, m, k);
+  int next = (p + 1) % m;
+  int rest_first = search(i + 1, j, next, m, k), rest_end = search(i, j - 1, next, m, k);
 
-	if (p == k)
-		return memo[i][j] = std::max(v[i] + rest_first, v[j] + rest_end);
-	else
-		return memo[i][j] = std::min(rest_first, rest_end);
+  if (p == k)
+    return memo[i][j] = std::max(v[i] + rest_first, v[j] + rest_end);
+  else
+    return memo[i][j] = std::min(rest_first, rest_end);
 }
 
 
 void testcase() {
-	int n, m, k; std::cin >> n >> m >> k;
-	std::fill(v.begin(), v.end(), 0);
-	for (auto &m: memo)
-		std::fill(m.begin(), m.end(), -1);
+  int n, m, k; std::cin >> n >> m >> k;
+  for (int i = 0; i <= n; i++)
+    memset(&memo[i], -1, n * sizeof(int));
 
-	for (int i = 0; i < n; ++i)
-		std::cin >> v[i];
-	
-	std::cout << search(0, n - 1, 0, m, k) << std::endl;
+  for (int i = 0; i < n; ++i)
+    std::cin >> v[i];
+  
+  std::cout << search(0, n - 1, 0, m, k) << std::endl;
 }
 
 int main() {
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
 
-	int t;
-	std::cin >> t;
-	for (int i = 0; i < t; ++i)
-		testcase();
+  int t;
+  std::cin >> t;
+  for (int i = 0; i < t; ++i)
+    testcase();
 }
