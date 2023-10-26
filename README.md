bb3e9aa146395f38b3225b205d543f01fab3baa1
dede1751 <63297263+dede1751@users.noreply.github.com>
2023-10-26 19:56:08 +0200

    London

--- README.md
+++ README.md
@@ -35,6 +35,6 @@ is guaranteed. The script is meant to be used as specified by its documentation.
 | [Boats](Week5/Boats/this.pdf)                                  | [boats.cpp](Week5/Boats/src/algorithm.cpp)                                      | 100       | Week 5    | Greedy                   |
 | [Motorcycles](Week6/Motorcycles/this.pdf)                      | [motorcycles.cpp](Week6/Motorcycles/src/algorithm.cpp)                          | 100       | POTW 6    | CGAL geometry            |
 | [Tiles](Week6/Tiles/this.pdf)                                  | [tiles.cpp](Week6/Tiles/src/algorithm.cpp)                                      | 100       | Week 6    | MaxFlow                  |
-| [London](Week6/London/this.pdf)                                | [london.cpp](Week6/London/src/algorithm.cpp)                                    |           | Week 6    |                          |
+| [London](Week6/London/this.pdf)                                | [london.cpp](Week6/London/src/algorithm.cpp)                                    | 100       | Week 6    | MaxFlow                  |
 | [CoinTossingTournament](Week6/CoinTossingTournament/this.pdf)  | [coin_tossing_tournament.cpp](Week6/CoinTossingTournament/src/algorithm.cpp)    |           | Week 6    |                          |
 | [Knights](Week6/Knights/this.pdf)                              | [knights.cpp](Week6/Knights/src/algorithm.cpp)                                  |           | Week 6    |                          |
---
+++ Week6/London/src/algorithm.cpp
@@ -0,0 +1,93 @@
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/push_relabel_max_flow.hpp>
+
+// Graph Type with nested interior edge properties for flow algorithms
+typedef boost::adjacency_list_traits<boost::vecS, boost::vecS, boost::directedS> traits;
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::no_property,
+    boost::property<boost::edge_capacity_t, long,
+        boost::property<boost::edge_residual_capacity_t, long,
+            boost::property<boost::edge_reverse_t, traits::edge_descriptor>>>> graph;
+
+typedef traits::vertex_descriptor vertex_desc;
+typedef traits::edge_descriptor edge_desc;
+
+// Custom edge adder class, highly recommended
+class edge_adder {
+	graph &G;
+
+	public:
+	explicit edge_adder(graph &G) : G(G) {}
+
+	void add_edge(int from, int to, long capacity) {
+		auto c_map = boost::get(boost::edge_capacity, G);
+		auto r_map = boost::get(boost::edge_reverse, G);
+		const auto e = boost::add_edge(from, to, G).first;
+		const auto rev_e = boost::add_edge(to, from, G).first;
+		c_map[e] = capacity;
+		c_map[rev_e] = 0; // reverse edge has no capacity!
+		r_map[e] = rev_e;
+		r_map[rev_e] = e;
+	}
+};
+
+void testcase() {
+	int h, w; std::cin >> h >> w;
+	std::vector<std::vector<char>> front(h, std::vector<char>(w)), back(h, std::vector<char>(w));
+	int in_pairs[26][26] = {0};
+	int out_occs[26] = {0};
+
+	std::string line; std::cin >> line;
+	for (auto c: line)
+		out_occs[c - 'A']++;
+
+	for (int i = 0; i < h; i++)
+		for (int j = 0; j < w; j++) {
+			char x; std::cin >> x;
+			front[i][j] = x - 'A';
+		}
+	
+	for (int i = 0; i < h; i++)
+		for (int j = w - 1; j >= 0; j--) {
+			char x; std::cin >> x;
+			back[i][j] = x - 'A';
+		}
+	
+	for (int i = 0; i < h; i++)
+		for (int j = 0; j < w; j++) {
+			int f = front[i][j], b = back[i][j];
+			in_pairs[std::min(f, b)][std::max(f, b)]++;
+		}
+	
+	graph g(27*26 + 2);
+	edge_adder adder(g);
+	const int src = 27*26, dst = 27*26 + 1;
+	for (int f = 0; f < 26; f++)
+		for (int b = f; b < 26; b++) {
+			adder.add_edge(src, f * 26 + b, in_pairs[f][b]);
+			adder.add_edge(f * 26 + b, 26 * 26 + f, in_pairs[f][b]);
+			adder.add_edge(f * 26 + b, 26 * 26 + b, in_pairs[f][b]);
+		}
+	
+	for (int i = 0; i < 26; i++)
+		adder.add_edge(26 * 26 + i, dst, out_occs[i]);
+
+	long flow = boost::push_relabel_max_flow(g, src, dst);
+	std::cout << ((flow == (long)line.length()) ? "Yes" : "No") << std::endl;
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+
+	int t;
+	std::cin >> t;
+	for (int i = 0; i < t; ++i)
+		testcase();
+}
