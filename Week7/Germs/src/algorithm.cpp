74733d3815d368149f8c5a07232a71bf03f1f531
dede1751 <63297263+dede1751@users.noreply.github.com>
2023-11-06 12:21:35 +0100

    Better Germs

--- Week7/Germs/src/algorithm.cpp
+++ Week7/Germs/src/algorithm.cpp
@@ -9,15 +9,10 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/nearest_neighbor_delaunay_2.h>
 #include <CGAL/Delaunay_triangulation_2.h>
-#include <CGAL/Triangulation_face_base_2.h>
-#include <CGAL/Triangulation_vertex_base_with_info_2.h>
 
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 using P = K::Point_2;
-using Vb = CGAL::Triangulation_vertex_base_with_info_2<int, K>;
-using Fb = CGAL::Triangulation_face_base_2<K>;
-using Tds = CGAL::Triangulation_data_structure_2<Vb,Fb>;
-using Triangulation = CGAL::Delaunay_triangulation_2<K, Tds>;
+using Triangulation = CGAL::Delaunay_triangulation_2<K>;
 
 int time_of_death(K::FT& square_dist)
 {
@@ -32,51 +27,32 @@ void testcase(int n)
 {
 	long l, b, r, t; std::cin >> l >> b >> r >> t;
 	Triangulation tr;
-	std::vector<std::pair<P, int>> points(n);
+	std::vector<P> points(n);
 
 	for (int i = 0; i < n; i++) {
 		int x, y; std::cin >> x >> y;
-		points[i] = std::make_pair(P(x, y), i);
+		points[i] = P(x, y);
 	}
 
 	tr.insert(points.begin(), points.end());
 	
-	std::vector<std::tuple<K::FT, int, int>> dists;
+	std::vector<K::FT> dists;
 	for (auto vh: tr.finite_vertex_handles()) {
 		auto neighbor = CGAL::nearest_neighbor(tr, vh);
-		auto n_dist = CGAL::squared_distance(vh->point(), neighbor->point());
+		long n_dist = CGAL::squared_distance(vh->point(), neighbor->point());
 
 		long x = vh->point().x(), y = vh->point().y();
 		long r_dist = std::min(r - x, std::min(x - l, std::min(t - y, y - b)));
 		r_dist = 4 * r_dist * r_dist;
 
-		if (r_dist > n_dist)
-			dists.push_back(std::make_tuple(n_dist, vh->info(), neighbor->info()));
-		else
-			dists.push_back(std::make_tuple(r_dist, vh->info(), n));
+		dists.push_back(CGAL::min(r_dist, n_dist));
 	}
 
 	std::sort(dists.begin(), dists.end());
 
-	int eliminated = 0, first = -1, half = -1, last = -1;
-	std::vector<bool> alive(n + 1, true);
-	alive[n] = false;
-
-	for (auto &i: dists) {
-		auto d = std::get<0>(i);
-		int src = std::get<1>(i), tgt = std::get<2>(i);
-
-		if (eliminated == 0)
-			first = time_of_death(d);
-
-		eliminated += alive[src] + alive[tgt];
-		alive[src] = alive[tgt] = false;
-
-		if (eliminated > n / 2 && half == -1)
-			half = time_of_death(d);
-		else if (eliminated == n)
-			last = time_of_death(d);
-	}
+	int first = time_of_death(dists[0]);
+	int half = time_of_death(dists[n / 2]);
+	int last = time_of_death(dists[n - 1]);
 
 	std::cout << first << " " << half << " " << last << std::endl;
 }
